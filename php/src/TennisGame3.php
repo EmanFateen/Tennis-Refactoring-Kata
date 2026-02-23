27df06e5458c3e3c6d35a70c61bc01f5463249e3
Eman Fateen <emanfateen@gmail.com>
2026-02-24 01:49:37 +0200

    chore: rename variables

--- php/src/TennisGame3.php
+++ php/src/TennisGame3.php
@@ -6,8 +6,8 @@ namespace TennisGame;
 
 class TennisGame3 implements TennisGame
 {
-    private int $score1 = 0;
-    private int $score2 = 0;
+    private int $firstPlayerScore = 0;
+    private int $secondPlayerScore = 0;
     private array $scoreTypes =  ['Love', 'Fifteen', 'Thirty', 'Forty'];
 
     public function __construct(
@@ -18,9 +18,9 @@ class TennisGame3 implements TennisGame
 
     public function getScore(): string
     {
-        if ($this->score1 < 4 && $this->score2 < 4 && !$this->totalScoreIsSix()) {
-            $firstPlayerScore = $this->scoreTypes[$this->score1];
-            $secondPlayerScore = $this->scoreTypes[$this->score2];
+        if ($this->firstPlayerScore < 4 && $this->secondPlayerScore < 4 && !$this->totalScoreIsSix()) {
+            $firstPlayerScore = $this->scoreTypes[$this->firstPlayerScore];
+            $secondPlayerScore = $this->scoreTypes[$this->secondPlayerScore];
 
             return $this->isDeuce() ?
                 "{$firstPlayerScore}-All" :
@@ -35,18 +35,18 @@ class TennisGame3 implements TennisGame
 
     public function wonPoint(string $playerName): void
     {
-        if ($playerName === 'player1')  $this->score1++;
-        if ($playerName === 'player2') $this->score2++;
+        if ($playerName === 'player1')  $this->firstPlayerScore++;
+        if ($playerName === 'player2') $this->secondPlayerScore++;
     }
 
     private function isDeuce(): bool
     {
-        return $this->score1 === $this->score2;
+        return $this->firstPlayerScore === $this->secondPlayerScore;
     }
 
     private function getWinner(): string
     {
-        $winnerName = $this->score1 > $this->score2 ?
+        $winnerName = $this->firstPlayerScore > $this->secondPlayerScore ?
             $this->firstPlayerName :
             $this->secondPlayerName;
 
@@ -57,11 +57,11 @@ class TennisGame3 implements TennisGame
 
     private function isAdvantage(): bool
     {
-        return ($this->score1 - $this->score2) * ($this->score1 - $this->score2) === 1;
+        return ($this->firstPlayerScore - $this->secondPlayerScore) * ($this->firstPlayerScore - $this->secondPlayerScore) === 1;
     }
 
     private function totalScoreIsSix(): bool
     {
-        return $this->score1 + $this->score2 === 6;
+        return $this->firstPlayerScore + $this->secondPlayerScore === 6;
     }
 }
