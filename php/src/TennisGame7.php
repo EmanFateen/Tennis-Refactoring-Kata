832ed7cb7e904a49d0fd2f2597ecfd08005f8220
Eman Fateen <emanfateen@gmail.com>
2026-02-26 20:45:43 +0200

    chore: reorder code

--- php/src/TennisGame7.php
+++ php/src/TennisGame7.php
@@ -45,6 +45,29 @@ class TennisGame7 implements TennisGame {
         return $result . ", enjoy your game!";
     }
 
+    private function isEndGame(): bool
+    {
+        return $this->firstPlayerScore >= 4 || $this->secondPlayerScore >= 4;
+    }
+
+    private function calculateEndGameScore(): string
+    {
+        $result = "Current score: ";
+
+        // end-game score
+        if ($this->firstPlayerScore - $this->secondPlayerScore === 1) {
+            $result .= "Advantage " . $this->firstPlayerName;
+        } elseif ($this->firstPlayerScore - $this->secondPlayerScore === -1) {
+            $result .= "Advantage " . $this->secondPlayerName;
+        } elseif ($this->firstPlayerScore - $this->secondPlayerScore >= 2) {
+            $result .= "Win for " . $this->firstPlayerName;
+        } else {
+            $result .= "Win for " . $this->secondPlayerName;
+        }
+
+        return $result . ", enjoy your game!";
+    }
+
     private function calculateNormalScore(): string
     {
         $result = "Current score: ";
@@ -64,31 +87,5 @@ class TennisGame7 implements TennisGame {
         };
 
         return $result . ", enjoy your game!";
-    }
-
-    /**
-     * @return string
-     */
-    public function calculateEndGameScore(): string
-    {
-        $result = "Current score: ";
-
-        // end-game score
-        if ($this->firstPlayerScore - $this->secondPlayerScore === 1) {
-            $result .= "Advantage " . $this->firstPlayerName;
-        } elseif ($this->firstPlayerScore - $this->secondPlayerScore === -1) {
-            $result .= "Advantage " . $this->secondPlayerName;
-        } elseif ($this->firstPlayerScore - $this->secondPlayerScore >= 2) {
-            $result .= "Win for " . $this->firstPlayerName;
-        } else {
-            $result .= "Win for " . $this->secondPlayerName;
-        }
-
-        return $result . ", enjoy your game!";
-    }
-
-    private function isEndGame(): bool
-    {
-        return $this->firstPlayerScore >= 4 || $this->secondPlayerScore >= 4;
     }
 }
